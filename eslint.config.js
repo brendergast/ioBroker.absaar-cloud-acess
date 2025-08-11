ab592af0a8fd2cd93291a0bfdc1764779a776808
brendergast <74229880+brendergast@users.noreply.github.com>
2025-08-11 18:31:41 +0000

    changed

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,37 @@
-import globals from "globals";
-import js from "@eslint/js";
+import globals from 'globals';
+import js from '@eslint/js';
 
 export default [
-  js.configs.recommended,
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.es2021,
-        // ioBroker Skript-Umgebung
-        getState: "readonly",
-        setState: "readonly",
-        setStateAsync: "readonly",
-        createState: "readonly",
-        schedule: "readonly",
-        log: "readonly"
-      }
-    },
-    rules: {
-      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-      "no-undef": "error",
-      "semi": ["error", "always"],
-      "quotes": ["error", "single"],
-      "indent": ["error", 4]
+    js.configs.recommended,
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.es2021,
+                // ioBroker Skript-Umgebung
+                getState: 'readonly',
+                setState: 'readonly',
+                setStateAsync: 'readonly',
+                createState: 'readonly',
+                schedule: 'readonly',
+                log: 'readonly',
+
+                // Für Tests:
+                describe: 'readonly',
+                it: 'readonly',
+                before: 'readonly',
+                after: 'readonly',
+                beforeEach: 'readonly',
+                afterEach: 'readonly',
+            }
+        },
+        rules: {
+            'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+            'no-undef': 'error',
+            'semi': ['error', 'always'],
+            'quotes': ['error', 'single'],
+            'indent': ['error', 4]
+        }
     }
-  }
 ];
